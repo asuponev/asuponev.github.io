ddcd5343b54823ad55d7ea0cf00294dc73ae80f1
Andrew <andrew.suponev@gmail.com>
2023-03-30 19:48:11 +0400

    fix: transfer images to the cloud

--- src/data/education-data.js
+++ src/data/education-data.js
@@ -1,9 +1,3 @@
-import cert_university from '../assets/img/certificates/university.jpg'
-import cert_python from '../assets/img/certificates/stepik_python.jpg';
-import cert_html from '../assets/img/certificates/stepik_html-css.jpg';
-import cert_js from '../assets/img/certificates/stepik_js.jpg';
-import cert_rss0 from '../assets/img/certificates/rss_stage_0.jpg';
-
 const educationData = [
   {
     id: "1",
@@ -11,7 +5,7 @@ const educationData = [
     company: "University",
     position: "Transport construction engineer",
     description: "Higher education at St. Petersburg State Transport University (cum laude)",
-    certificate: cert_university
+    certificate: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/certificates%2Funiversity.jpg?alt=media&token=145561ef-de53-403f-9760-1c3f1fd8fcea"
   },
   {
     id: "2",
@@ -19,7 +13,7 @@ const educationData = [
     company: "Stepik.org",
     position: "Junior Python Developer",
     description: "Course «Programming on Python»",
-    certificate: cert_python
+    certificate: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/certificates%2Fstepik_python.jpg?alt=media&token=f4067001-8891-41cf-b5ec-938440eeccb4"
   },
   {
     id: "3",
@@ -27,7 +21,7 @@ const educationData = [
     company: "Stepik.org",
     position: "Junior Front-End Developer",
     description: "Course «Web dev for beginner: HTML & CSS»",
-    certificate: cert_html
+    certificate: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/certificates%2Fstepik_html-css.jpg?alt=media&token=0251e64f-7e6b-4961-947a-9ddf75aee159"
   },
   {
     id: "4",
@@ -35,7 +29,7 @@ const educationData = [
     company: "Stepik.org",
     position: "Junior Front-End Developer",
     description: "Course «JavaScript for beginner»",
-    certificate: cert_js
+    certificate: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/certificates%2Fstepik_js.jpg?alt=media&token=53785367-bf48-48dc-a1ef-3041b3d8d120"
   },
   {
     id: "5",
@@ -43,7 +37,7 @@ const educationData = [
     company: "RS School",
     position: "Junior Software Engineer",
     description: "Stage 0 of the course «JavaScript/Front-end»",
-    certificate: cert_rss0
+    certificate: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/certificates%2Frss_stage_0.jpg?alt=media&token=e227a97b-4e5c-459b-b645-0a4a170014ec"
   },
   {
     id: "6",
--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,9 +1,3 @@
-import cover_gamesnake from '../assets/img/projects/cover_game-snake.jpg';
-import cover_audioplayer from '../assets/img/projects/cover_audio-player.jpg';
-import cover_portfolio from '../assets/img/projects/cover_portfolio-site.jpg';
-import cover_plants from '../assets/img/projects/cover_plants.jpg';
-import cover_icollect from '../assets/img/projects/cover_icollect.jpg';
-
 const projectsData = [
   {
     id: "1",
@@ -11,7 +5,7 @@ const projectsData = [
     description: "Classic snake game with dynamic difficulty, game counter and memorization of results with the selection of the best",
     note: "Task from stage 0 of The Rolling Scopes School",
     skills: ["HTML", "CSS", "JavaScript"],
-    cover: cover_gamesnake,
+    cover: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/projects%2Fcover_game-snake.jpg?alt=media&token=3cb631d7-12f4-4454-b5f2-89e1b3391380",
     git: "https://github.com/elsuppo/JSFEPRESCHOOL2022Q1/tree/game-snake",
     deploy: "https://elsuppo.github.io/JSFEPRESCHOOL2022Q1/game-snake"
   },
@@ -21,7 +15,7 @@ const projectsData = [
     description: "Music player that allows you to play music tracks in turn or flip through them. Each music track has a specific cover",
     note: "Task from stage 0 of The Rolling Scopes School",
     skills: ["HTML", "CSS", "JavaScript"],
-    cover: cover_audioplayer,
+    cover: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/projects%2Fcover_audio-player.jpg?alt=media&token=55b693ec-46e7-44be-876b-09d8bb872a2d",
     git: "https://github.com/elsuppo/JSFEPRESCHOOL2022Q1/tree/audio-player",
     deploy: "https://elsuppo.github.io/JSFEPRESCHOOL2022Q1/audio-player"
   },
@@ -31,7 +25,7 @@ const projectsData = [
     description: "The photographer's portfolio landing page made adaptive and interactive",
     note: "Task from stage 0 of The Rolling Scopes School",
     skills: ["HTML", "CSS", "JavaScript"],
-    cover: cover_portfolio,
+    cover: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/projects%2Fcover_portfolio-site.jpg?alt=media&token=82075880-afdf-4a7f-b5b9-b3adf5b43ccf",
     git: "https://github.com/elsuppo/JSFEPRESCHOOL2022Q1/tree/portfolio",
     deploy: "https://elsuppo.github.io/JSFEPRESCHOOL2022Q1/portfolio"
   },
@@ -41,7 +35,7 @@ const projectsData = [
     description: "Landing page for a landscaping company",
     note: "Task from stage 0 of The Rolling Scopes School",
     skills: ["HTML", "CSS", "JavaScript"],
-    cover: cover_plants,
+    cover: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/projects%2Fcover_plants.jpg?alt=media&token=0bdb5d70-b8d8-4525-9a56-707bfd741035",
     git: "https://github.com/elsuppo/JSFEPRESCHOOL2022Q4/tree/main/plants",
     deploy: "https://elsuppo.github.io/JSFEPRESCHOOL2022Q4/plants/dist/"
   },
@@ -51,7 +45,7 @@ const projectsData = [
     description: "Wep Application for managing personal collections",
     note: "The coursework as part of the internship",
     skills: ["Node.js", "Express.js", "Mongo DB", "React", "Redux", "Material UI"],
-    cover: cover_icollect,
+    cover: "https://firebasestorage.googleapis.com/v0/b/helloitsme-eac50.appspot.com/o/projects%2Fcover_icollect.jpg?alt=media&token=c52a3970-0182-4d83-b4b9-b0dac5cf028c",
     git: "https://github.com/elsuppo/iCollect",
     deploy: "https://icollect.onrender.com"
   },
