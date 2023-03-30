50f68f175aebb29abae6466619c9e3513f4f3679
Andrew <andrew.suponev@gmail.com>
2023-03-30 17:42:12 +0400

    fix: disable devTools for production

--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import options from './slices/options';
 
 export const store = configureStore({
-  reducer: { options }
+  reducer: { options },
+  devTools: process.env.NODE_ENV !== 'production'
 });
