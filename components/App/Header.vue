800cd48532a1babdb1f3831dc7817b41962e2139
Andrew <andrew.suponev@gmail.com>
2023-06-18 13:04:39 +0400

    refactor: replace header elements to Components-App folder

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <header class="fixed top-0 z-50 w-full bg-white dark:bg-gray-900">
+  <header class="fixed top-0 z-40 w-full bg-white dark:bg-gray-900">
     <div class="container">
       <div class="flex h-16 items-center justify-between">
         <div>
-          <AppHeaderLogo />
+          <AppLogo />
         </div>
         <div>
-          <AppHeaderThemeSwitcher />
+          <AppThemeSwitcher />
         </div>
       </div>
     </div>
